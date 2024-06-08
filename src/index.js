f3af81bef872ec1c808ac8d540039a569365ac2c
Abhinandan meena <143181729+Nandu8821@users.noreply.github.com>
2024-06-08 09:22:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="https://chatting-backend-gamma.vercel.app"
+export const BASE_URL="https://chatting-backend-v4y0.onrender.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
